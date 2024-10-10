ea419427baafda10eb1fab9ce13c19462718843b
vivekChenna <chennavivek96@gmail.com>
2024-10-10 14:55:28 +0530

    build club hume ai

---
+++ components/Nav.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { useLayoutEffect, useState } from "react";
+import HumeLogo from "./logos/Hume";
+import { Button } from "./ui/button";
+import { Moon, Sun } from "lucide-react";
+import Github from "./logos/GitHub";
+import pkg from "@/package.json";
+import Image from "next/image";
+
+export const Nav = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useLayoutEffect(() => {
+    const el = document.documentElement;
+
+    if (el.classList.contains("dark")) {
+      setIsDarkMode(true);
+    } else {
+      setIsDarkMode(false);
+    }
+  }, []);
+
+  const toggleDark = () => {
+    const el = document.documentElement;
+    el.classList.toggle("dark");
+    setIsDarkMode((prev) => !prev);
+  };
+
+  return (
+    <div
+      className={
+        "px-4 py-2 flex items-center h-16 z-50 bg-card border-b border-border"
+      }
+    >
+      <div>
+        {/* <HumeLogo className={"h-5 w-auto"} /> */}
+        <Image
+          src={`/build_club.png`}
+          alt="BuildClub"
+          height={50}
+          width={50}
+        />
+      </div>
+      <div className={"ml-auto flex items-center gap-1"}>
+        <Button
+          onClick={toggleDark}
+          variant={"ghost"}
+          className={"ml-auto flex items-center gap-1.5"}
+        >
+          <span>
+            {isDarkMode ? (
+              <Sun className={"size-4"} />
+            ) : (
+              <Moon className={"size-4"} />
+            )}
+          </span>
+          <span>{isDarkMode ? "Light" : "Dark"} Mode</span>
+        </Button>
+      </div>
+    </div>
+  );
+};
